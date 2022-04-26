5fbdd0b7309d8f0a08ea8db278a30d437a844132
Ali Bashir <ali.bashir@blueeast.com>
2022-04-26 15:20:00 +0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,114 @@
+provider "aws" {
+  shared_config_files      = ["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
+}
+
+#front-end ec2 instance
+module "ec2_instance-1" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "frontend-instance"
+
+  ami                    = "ami-0a8dc52684ee2fee2"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = false
+  vpc_security_group_ids = [module.frontend_service_sg.security_group_id]
+  subnet_id              = aws_subnet.front-end.id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "development"
+  }
+}
+
+#back-end ec2 instance
+module "ec2_instance-2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "backend-instance"
+
+  ami                    = "ami-0a8dc52684ee2fee2"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = false
+  vpc_security_group_ids = [module.backend_service_sg.security_group_id]
+  subnet_id              = aws_subnet.back-end.id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "development"
+  }
+}
+
+#database ec2 instance
+module "ec2_instance-3" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "database-instance"
+
+  ami                    = "ami-0a8dc52684ee2fee2"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = false
+  vpc_security_group_ids = [module.database_service_sg.security_group_id]
+  subnet_id              = aws_subnet.database.id
+
+  tags = {
+    Terraform   = "true"
+    Environment = "development"
+  }
+}
+
+
+#vpc
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "development"
+  }
+}
+
+resource "aws_subnet" "front-end" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "fron-end-private-subnet"
+  }
+}
+
+resource "aws_subnet" "back-end" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name = "back-end-private-subnet"
+  }
+
+}
+
+resource "aws_subnet" "database" {
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-west-1c"
+
+  tags = {
+    Name = "database-private-subnet"
+  }
+
+}
---
+++ security_group.tf
@@ -0,0 +1,90 @@
+#security group for frontend service
+module "frontend_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "Fornt-end-service"
+  description = "Security group for user-service with port 443"
+  vpc_id      = module.vpc.vpc_id
+
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
+#inbound
+resource "aws_security_group_rule" "https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["10.10.0.0/16"]
+  security_group_id = module.frontend_service_sg.security_group_id
+}
+#outbound
+resource "aws_security_group_rule" "https_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.frontend_service_sg.security_group_id
+}
+#security group for backend service
+
+module "backend_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "backend-end-service"
+  description = "Security group for user-service with port 8080"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = [module.frontend_service_sg.security_group_id]
+
+
+}
+#inbound
+resource "aws_security_group_rule" "proxy" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = module.frontend_service_sg.security_group_id
+  security_group_id        = module.backend_service_sg.security_group_id
+}
+#outbound
+resource "aws_security_group_rule" "proxy_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.backend_service_sg.security_group_id
+}
+#security group for database
+
+module "database_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name                = "database-service"
+  description         = "Security group for user-service with default mysql"
+  vpc_id              = module.vpc.vpc_id
+  ingress_cidr_blocks = [module.backend_service_sg.security_group_id]
+
+}
+#inbound rule
+resource "aws_security_group_rule" "mysql" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "-1"
+  source_security_group_id = module.backend_service_sg.security_group_id
+  security_group_id        = module.database_service_sg.security_group_id
+}
+#outbound rule
+resource "aws_security_group_rule" "mysql_out" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  cidr_blocks       = ["0.0.0.0/0"]
+  protocol          = "-1"
+  security_group_id = module.database_service_sg.security_group_id
+}
+
